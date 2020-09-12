e12abbe0b1efcb92fd4354a40811620d51446b7b
jumpeiMano <jumpeim37@gmail.com>
2020-09-12 15:43:52 +0900

    change unique index

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -45,11 +45,11 @@ CREATE TABLE isuumo.chair_features
     chair_id    INTEGER         NOT NULL,
     feature_id  INTEGER         NOT NULL
 );
-CREATE INDEX chair_features_fc ON isuumo.chair_features (feature_id, chair_id);
+CREATE UNIQUE INDEX chair_features_fc ON isuumo.chair_features (feature_id, chair_id);
 
 CREATE TABLE isuumo.estate_features
 (
     estate_id   INTEGER         NOT NULL,
     feature_id  INTEGER         NOT NULL
 );
-CREATE INDEX estate_features_fe ON isuumo.estate_features (feature_id, estate_id);
+CREATE UNIQUE INDEX estate_features_fe ON isuumo.estate_features (feature_id, estate_id);
