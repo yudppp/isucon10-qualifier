8499589163001299be7acbc5850b3bc9ca44817d
yudppp <yu.d.ppp@gmail.com>
2020-09-12 19:13:32 +0900

    move 04 sql

--- webapp/mysql/db/4_AddGeo.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE isuumo.estate ADD geo GEOMETRY;
-UPDATE isuumo.estate SET estate.`geo` = GeomFromText(Concat('POINT(',estate.latitude,' ',estate.longitude,')'));
-ALTER TABLE isuumo.estate MODIFY geo GEOMETRY NOT NULL;
-CREATE SPATIAL INDEX estate_door ON isuumo.estate (geo);
