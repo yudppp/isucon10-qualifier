e1707ba5aeaac98fbefefce706b57291aff3ef27
Yusaku Matsuki <ymatzki@gmail.com>
2020-09-12 19:53:22 +0900

    create user

--- webapp/mysql/db/init.sh
+++ webapp/mysql/db/init.sh
@@ -11,4 +11,4 @@ export MYSQL_PWD=${MYSQL_PASS:-isucon}
 export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
-cat 0_Schema.sql 1_DummyEstateData.sql 2_DummyChairData.sql 3_DummyFeatures.sql 4_AddGeo.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 0_Schema.sql 1_DummyEstateData.sql 2_DummyChairData.sql 3_DummyFeatures.sql 4_AddGeo.sql 5_CreateUser.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
