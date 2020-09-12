46d40d119a59e449f069dd02ad34e8fb436e33b4
yudppp <yu.d.ppp@gmail.com>
2020-09-12 15:37:59 +0900

    add: Add dummy features

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -43,11 +43,11 @@ CREATE TABLE isuumo.chair_features
     chair_id    INTEGER         NOT NULL,
     feature_id  INTEGER         NOT NULL
 );
-CREATE INDEX chair_features_fc ON chair_features (feature_id, chair_id);
+CREATE INDEX chair_features_fc ON isuumo.chair_features (feature_id, chair_id);
 
 CREATE TABLE isuumo.estate_features
 (
     estate_id   INTEGER         NOT NULL,
     feature_id  INTEGER         NOT NULL
 );
-CREATE INDEX chair_features_fe ON estate_features (feature_id, estate_id);
+CREATE INDEX chair_features_fe ON isuumo.estate_features (feature_id, estate_id);
