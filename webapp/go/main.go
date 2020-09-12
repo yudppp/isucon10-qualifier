0e75913949ee9e0d64c16de8b8c025036fbd4089
jumpeiMano <jumpeim37@gmail.com>
2020-09-12 18:35:43 +0900

    tuning query

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -478,7 +478,7 @@ func searchChairs(c echo.Context) error {
 
 	if c.QueryParam("features") != "" {
 		for i, f := range strings.Split(c.QueryParam("features"), ",") {
-			join += fmt.Sprintf("inner join chair_features cf%d on id = cf%d.chair_id and cf%d.feature_id =  ? ", i)
+			join += fmt.Sprintf("inner join chair_features cf%d on id = cf%d.chair_id and cf%d.feature_id =  ? ", i, i, i)
 			params = append(params, estateFeaturesMap[f])
 		}
 	}
@@ -851,7 +851,7 @@ func searchEstates(c echo.Context) error {
 
 	if c.QueryParam("features") != "" {
 		for i, f := range strings.Split(c.QueryParam("features"), ",") {
-			join += fmt.Sprintf("inner join estate_features ef%d on id = ef%d.estate_id and ef%d.feature_id =  ? ", i)
+			join += fmt.Sprintf("inner join estate_features ef%d on id = ef%d.estate_id and ef%d.feature_id =  ? ", i, i, i)
 			params = append(params, estateFeaturesMap[f])
 		}
 	}
